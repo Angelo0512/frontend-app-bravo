6703afa6733d8a45f3674c057d943cddb83e8b98
ArnoldG6 <arnoldgq612@gmail.com>
2022-04-19 21:26:27 -0600

    MainFragment added, icons need to be implemented

---
+++ app/src/main/java/cr/una/bravo/bravofrontend/ui/MainMenuFragmentAdapter.kt
@@ -0,0 +1,51 @@
+package cr.una.bravo.bravofrontend.ui
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import cr.una.bravo.bravofrontend.R
+
+import cr.una.bravo.bravofrontend.ui.placeholder.PlaceholderContent.PlaceholderItem
+import cr.una.bravo.bravofrontend.ui.databinding.FragmentMainMenuBinding
+
+/**
+ * [RecyclerView.Adapter] that can display a [PlaceholderItem].
+ * TODO: Replace the implementation with code for your data type.
+ */
+class MainMenuFragmentAdapter(
+    private val values: List<PlaceholderItem>
+) : RecyclerView.Adapter<MainMenuFragmentAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(
+            FragmentMainMenuBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        holder.idView.text = item.id
+        holder.contentView.text = item.content
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(binding: FragmentMainMenuBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val idView: TextView = binding.itemNumber
+        val contentView: TextView = binding.content
+
+        override fun toString(): String {
+            return super.toString() + " '" + contentView.text + "'"
+        }
+    }
+
+}

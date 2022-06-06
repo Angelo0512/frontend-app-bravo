0ff79995318b66a986be5631a008e272d527587a
AraR11 <aramisramirez19@gmail.com>
2022-06-06 16:58:50 -0600

    Report model

--- app/src/main/java/cr/una/bravo/bravofrontend/model/Report.kt
+++ app/src/main/java/cr/una/bravo/bravofrontend/model/Report.kt
@@ -2,3 +2,12 @@ package cr.una.bravo.bravofrontend.model
 
 import java.util.*
 
+data class Report (
+    var id: Long,
+    var creationDate: Date,
+    var description: String,
+    var services: List<Service>,
+    var client: UserBasic,
+    var vehicle: Vehicle,
+    var technician: UserBasic,
+)

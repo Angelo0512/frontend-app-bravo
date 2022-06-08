ac078089af40386c8e958342f7bae53bc75d1588
AraR11 <aramisramirez19@gmail.com>
2022-06-07 20:49:12 -0600

    mvvm NewVehicleFragment

--- app/src/main/java/cr/una/bravo/bravofrontend/view/vehicle/NewVehicleFragment.kt
+++ app/src/main/java/cr/una/bravo/bravofrontend/view/vehicle/NewVehicleFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
@@ -13,6 +14,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.Navigation
 import cr.una.bravo.bravofrontend.R
 import cr.una.bravo.bravofrontend.databinding.FragmentNewVehicleBinding
+import cr.una.bravo.bravofrontend.model.Vehicle
 import cr.una.bravo.bravofrontend.viewmodel.VehicleViewModel
 import cr.una.bravo.bravofrontend.viewmodel.VehicleViewModelFactory
 
@@ -29,7 +31,9 @@ class NewVehicleFragment : Fragment() {
 
     // View model
     private lateinit var vehicleViewModel: VehicleViewModel
-
+    //Select item of spinner
+    private lateinit var typeSelected: String
+    private lateinit var motorSelected: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,10 +42,11 @@ class NewVehicleFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding=  FragmentNewVehicleBinding.inflate(inflater,container, false)
 
-        // TaskViewModelFactory
+        // VehicleViewModelFactory
         vehicleViewModel =
             ViewModelProvider(this, VehicleViewModelFactory())[VehicleViewModel::class.java]
 
+        // Init spinners
         val spinnerTipo: Spinner = binding.fieldVehicleTipo
         val adapterTipo: ArrayAdapter<String> = ArrayAdapter<String>(
             activity?.applicationContext!!,
@@ -58,9 +63,24 @@ class NewVehicleFragment : Fragment() {
         )
         spinnerTipoMotor.adapter = adapterTipoMotor
 
-        binding.btnVehicleSubmitNew.setOnClickListener { findNavController().navigate(
-            R.id.action_insertVehicle_to_insertClientFragment
-        ) }
+
+        // Spinner select event
+        typeSelected = spinnerTipo.selectedItem.toString()
+        motorSelected = spinnerTipoMotor.selectedItem.toString()
+
+
+    binding.btnVehicleSubmitNew.setOnClickListener {
+        vehicleViewModel.createVehicle(
+            Vehicle(
+                plateNumber = binding.fieldVehiclePlaca.text.toString() ,
+                vinNumber = binding.fieldVehicleVIN.text.toString(),
+                brand = binding.fieldVehicleMarca.text.toString(),
+                motorSerial = binding.fieldVehicleSerie.text.toString(),
+                vehicleClass = typeSelected,
+                motorType= motorSelected,
+            )
+        )
+        findNavController().navigate(R.id.action_insertVehicle_to_insertClientFragment) }
 
         return binding.root
     }

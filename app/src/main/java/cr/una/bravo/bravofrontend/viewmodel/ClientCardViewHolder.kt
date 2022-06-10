1971b2e74dba4ba8e23f4fa7ea8694d2acc4bdd5
Haxiz <assassinsand@gmail.com>
2022-06-09 19:39:48 -0600

    fix shown data for client list

--- app/src/main/java/cr/una/bravo/bravofrontend/viewmodel/ClientCardViewHolder.kt
+++ app/src/main/java/cr/una/bravo/bravofrontend/viewmodel/ClientCardViewHolder.kt
@@ -3,14 +3,18 @@ package cr.una.bravo.bravofrontend.viewmodel
 import androidx.recyclerview.widget.RecyclerView
 import cr.una.bravo.bravofrontend.databinding.ClientListItemBinding
 import cr.una.bravo.bravofrontend.model.UserBasic
+import cr.una.bravo.bravofrontend.BuildConfig.DATE_FORMAT
+import java.text.SimpleDateFormat
 
 class ClientCardViewHolder(
     private val clientListItemBinding: ClientListItemBinding
-) : RecyclerView.ViewHolder(clientListItemBinding.root)
-{
-    fun bindClientCard(clientCard : UserBasic){
-        clientListItemBinding.clientId.text = clientCard.id.toString()
-        clientListItemBinding.clientName.text = clientCard.firstName + " " + clientCard.lastName
-        clientListItemBinding.clientEmail.text = clientCard.email
+) : RecyclerView.ViewHolder(clientListItemBinding.root) {
+    fun bindClientCard(clientCard: UserBasic) {
+        clientListItemBinding.clientId.text = "ID: " + clientCard.id.toString()
+        clientListItemBinding.clientName.text =
+            "Nombre: " + clientCard.firstName + " " + clientCard.lastName
+        clientListItemBinding.clientEmail.text = "Correo: " + clientCard.email
+        clientListItemBinding.clientEntryDate.text =
+            "Fecha de Ingreso: " + SimpleDateFormat(DATE_FORMAT).format(clientCard.createDate)
     }
 }

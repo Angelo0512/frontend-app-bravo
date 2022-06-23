3deb5201949bb19e6b3e59111934d4189c91a329
Haxiz <assassinsand@gmail.com>
2022-06-23 17:29:25 -0600

    Update ClientReportAdapter.kt

--- app/src/main/java/cr/una/bravo/bravofrontend/adapter/ClientReportAdapter.kt
+++ app/src/main/java/cr/una/bravo/bravofrontend/adapter/ClientReportAdapter.kt
@@ -2,7 +2,10 @@ package cr.una.bravo.bravofrontend.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import cr.una.bravo.bravofrontend.R
 import cr.una.bravo.bravofrontend.databinding.ClientItemBinding
 import cr.una.bravo.bravofrontend.databinding.VehicleItemBinding
 import cr.una.bravo.bravofrontend.model.UserBasic
@@ -11,6 +14,8 @@ import cr.una.bravo.bravofrontend.model.Vehicle
 class ClientReportAdapter : RecyclerView.Adapter<ClientViewHolder>(){
 
     private var user = mutableListOf<UserBasic>()
+    private val clickedPosition = -1
+    private val selectedItem = 0
 
 
     fun setUserList(userList: List<UserBasic>) {
@@ -26,7 +31,16 @@ class ClientReportAdapter : RecyclerView.Adapter<ClientViewHolder>(){
 
     override fun onBindViewHolder(holder: ClientViewHolder, position: Int) {
         holder.bindClient(user[position])
+        holder.clientListItemBinding.root.setOnClickListener {
+            val bundle = bundleOf(USER_ID to user[position].id.toString())
+            holder.clientListItemBinding.root.findNavController().navigate(
+                R.id.action_insertClientFragment_to_insertServiceFragment, bundle
+            )
+        }
     }
 
+    companion object {
+        const val USER_ID = "user_id"
+    }
     override fun getItemCount(): Int = user.size
 }
